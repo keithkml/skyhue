7644d95672b760e4338895e8bf7f91b466176ad3
Keith Lea <keithl@gmail.com>
2021-08-09 10:50:19 -0400

    Use sensors to control brightness

--- globe.js
+++ globe.js
@@ -4,8 +4,8 @@ let fetch = require("node-fetch")
 // Generate username using instructions at:
 // https://developers.meethue.com/develop/get-started-2/#so-lets-get-started
 const USERNAME = process.env.HUE_USERNAME
-const LIGHT_NAME = "Globe"
-const LOCATION = "40.6718891,-73.9728584"
+const LIGHT_NAME_PATTERN = "Globe.*"
+const LOCATION = "41.218217,-73.8720515"
 // Get a free Dark Sky API key at https://darksky.net/dev/docs
 const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY
 
@@ -51,26 +51,83 @@ const COLORS = {
 }
 
 // Global variables
+let sunrise = 0
+let sunset = 0
+let temperature = 0
 let client
-let lightId
+let lightIds = []
+let sensorIdsByUniqueId = {}
+let SENSOR_UNIQUE_IDS_BY_LIGHT_NAME = {
+  "Globe Keith office": "00:17:88:01:04:b6:89:68-02-0400",
+  "Globe bedroom": "00:17:88:01:06:f5:f1:d5-02-0400",
+}
+let LIGHT_FACTORS = {
+  "Globe Keith office": 0.6,
+  "Globe bedroom": 1.1,
+}
 
 function exitHorribly() {
   setColor({ on: false }).then(() => process.exit(1))
   setTimeout(() => process.exit(1), 5 * 1000)
 }
 
+function convertLightLevelToBrightnessFraction(lightLevelFromSensor) {
+  return lightLevelFromSensor / 25000
+}
+
 /**
  * @param obj {on: boolean, brightness: 0-254, hue: 0-65535, saturation: 0-254, transitionTime: 0-5}
  */
 function setColor(obj) {
-  return client.lights.getById(lightId).then(light => {
-    console.log("light was", light)
-    console.log("setting color", obj)
-    light.effect = "none"
-    light.transitionTime = 0
-    Object.assign(light, obj)
-    return client.lights.save(light)
-  })
+  return (
+    client &&
+    client.lights &&
+    Promise.all(
+      lightIds.map(lightId =>
+        client.lights.getById(lightId).then(light => {
+          console.log("light was", light)
+          const sensorUniqueId = SENSOR_UNIQUE_IDS_BY_LIGHT_NAME[light.attributes.attributes.name]
+          console.log("unique id: ", sensorUniqueId)
+          const sensorId = sensorIdsByUniqueId[sensorUniqueId]
+          console.log("sensor id: ", sensorId)
+          if (sensorId) {
+            return client.sensors.getById(sensorId).then(sensor => {
+              light.effect = "none"
+              light.transitionTime = 0
+              Object.assign(light, obj)
+              if ("lightlevel" in sensor.state.attributes.attributes) {
+                const level = sensor.state.attributes.attributes.lightlevel
+                console.log("light level for " + light.name + " is " + level)
+                let b = Math.round(convertLightLevelToBrightnessFraction(level) * 255)
+                if (light.name in LIGHT_FACTORS) {
+                  b *= LIGHT_FACTORS[light.name]
+                }
+                console.log("desired brightness before clamp ", b)
+                if (b > 255) {
+                  b = 255
+                }
+                if (b < 0) {
+                  b = 0
+                }
+                light.on = b > 0
+                light.brightness = b
+              } else {
+                console.log("sensor for " + light.name + " is missing lightlevel prop")
+              }
+              console.log("setting color after sensor", light)
+              return client.lights.save(light)
+            })
+          } else {
+            console.log("setting color without sensor", obj)
+            light.effect = "none"
+            light.transitionTime = 0
+            Object.assign(light, obj)
+            return client.lights.save(light)
+          }
+        })
+      )
+    )
+  )
 }
 
 function getColorForTemperature(temp) {
@@ -118,7 +175,7 @@ function setColorForTemperature(temp, brightness) {
   })
 }
 
-const isBeforeWakeupTime = () => new Date().getHours() < 6
+const isBeforeWakeupTime = () => new Date().getHours() < 7
 const isAfterBedtime = () => new Date().getHours() >= 21
 
 function getBrightness(position) {
@@ -140,29 +197,17 @@ function updateForecast() {
         return
       }
       res.json().then(weather => {
-        console.log(weather)
+        // console.log(weather)
         console.log("currently: " + weather.currently.summary)
-        console.log("minutely: " + weather.minutely.summary)
         console.log("daily: " + weather.daily.summary)
         //	    if (weather.precipProbability > 0 && weather.precipIntensity > 0) {
         //		setColorForRain()
         //	    } else {
         const today = weather.daily.data[0]
-        const time = Math.floor(new Date().getTime() / 1000)
-        const sunrise = today.sunriseTime
-        const sunset = today.sunsetTime
-        let positionInDaylightWindow = (time - sunrise) / (sunset - sunrise)
-        console.log("position in daylight window: " + positionInDaylightWindow)
-        //		if (positionInDaylightWindow < 0
-        if (isBeforeWakeupTime() || isAfterBedtime()) {
-          console.log("light should be off")
-          setColor({ on: false })
-        } else {
-          setColorForTemperature(
-            weather.currently.temperature,
-            getBrightness(positionInDaylightWindow)
-          )
-        }
+        sunrise = today.sunriseTime
+        sunset = today.sunsetTime
+        temperature = weather.currently.temperature
+        updateGlobes()
       })
     })
     .catch(error => {
@@ -171,10 +216,20 @@ function updateForecast() {
     })
 }
 
-function startWeatherCheck() {
+function updateGlobes() {
+  const time = Math.floor(new Date().getTime() / 1000)
+  // assume midnight if sunset/sunrise not set yet
+  let positionInDaylightWindow = sunrise && sunset ? (time - sunrise) / (sunset - sunrise) : 0
+  console.log("position in daylight window: " + positionInDaylightWindow)
+  //		if (positionInDaylightWindow < 0
+  setColorForTemperature(temperature, getBrightness(positionInDaylightWindow))
+}
+
+function startPolls() {
   console.log("Starting weather check loop")
   updateForecast()
   setInterval(updateForecast, 5 * 60 * 1000)
+  setInterval(updateGlobes, 5 * 1000)
 }
 
 huejay
@@ -187,10 +242,20 @@ huejay
       username: USERNAME,
     })
     client.lights.getAll().then(lights => {
-      const light = lights.find(l => l.name == LIGHT_NAME)
-      lightId = light.id
-      console.log("Found light", light)
-      startWeatherCheck()
+      const globes = lights.filter(l => l.name.match(LIGHT_NAME_PATTERN))
+      lightIds = globes.map(l => l.id)
+      console.log("Found lights", lights)
+      startPolls()
+    })
+    client.sensors.getAll().then(sensors => {
+      // Just print this to make it easier to manually update
+      const lightSensors = sensors.filter(s => s.type == "ZLLLightLevel")
+      console.log("all light sensors ")
+      console.dir(lightSensors, { depth: null })
+      lightSensors.forEach(
+        s => (sensorIdsByUniqueId[s.attributes.attributes.uniqueid] = s.attributes.attributes.id)
+      )
+      console.log(sensorIdsByUniqueId)
     })
   })
   .catch(error => {
